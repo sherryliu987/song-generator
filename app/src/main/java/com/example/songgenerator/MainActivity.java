3907a2e5aadc28b95d6830712e141110ff209aa9
Sherry <sherryliu987@gmail.com>
2021-05-04 13:24:19 -0400

    song plays wooo

--- app/src/main/java/com/example/songgenerator/MainActivity.java
+++ app/src/main/java/com/example/songgenerator/MainActivity.java
@@ -7,12 +7,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // hello sherry
-        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.keep_on);
+         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.keep_on);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         mediaPlayer.start(); // no need to call prepare(); create() does that for you
     }
 }
