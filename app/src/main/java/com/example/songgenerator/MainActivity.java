2ea6dd6f5c5046cdaa94eb05c039df48d5520d21
Sherry <sherryliu987@gmail.com>
2021-05-05 16:10:44 -0400

    plays first note??

--- app/src/main/java/com/example/songgenerator/MainActivity.java
+++ app/src/main/java/com/example/songgenerator/MainActivity.java
@@ -4,58 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    String sequence = "";
+    int[] notes = {R.raw.note1, R.raw.note2};
+    int i = 0;
+
     MediaPlayer mediaPlayer;
-    boolean note1 = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // hello sherry
+
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.note1);
+
         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
+                System.out.println("FINISHED PLAYING DA NOTEEEEE");
                 mediaPlayer.reset();
-                mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.note2);
-//                try {
-//                    String path = "android.resource://" + getPackageName() + "/" + R.raw.note2;
-//                    mediaPlayer.setDataSource(path);
-//                    System.out.println("WORKOOOOOOOOOOOOO");
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                    System.out.println("NO WORKOOOOOOOOOO");
-//                }
-                mediaPlayer.start();
+                i++;
+                if(i < sequence.length()){
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), notes[(sequence.charAt(i) - 'a') % 2]);
+                    mediaPlayer.start();
+                }
             }
         });
-    }
-    //hiiii
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mediaPlayer.start();
 
-//            if(note1){
-//
-//            }else{
-//                try {
-//                    mediaPlayer.setDataSource(String.valueOf(R.raw.note2));
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            note1 = !note1;
-//            mediaPlayer.start(); // no need to call prepare(); create() does that for you
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-        }
+    }
+
+    public void playSong(View view){
+
+        final EditText edit =  (EditText) findViewById(R.id.textInputEditText);
+        sequence = (String) edit.getText().toString();
+        sequence = sequence.toLowerCase();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), notes[(sequence.charAt(i) - 'a') % 2]);
+        mediaPlayer.start();
+    }
+
+//    //hiiii
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        mediaPlayer.start();
+//        }
 
 }
